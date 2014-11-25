c3d3ae084cdb27ba23ec6e8c0f2c285ef7e55e51
dgmp88 <dgmprichard@gmail.com>
2014-11-25 17:09:47 +0300

    Define segments and block width here

--- core/src/com/boondog/circulartetris/CircularTetris.java
+++ core/src/com/boondog/circulartetris/CircularTetris.java
@@ -6,6 +6,9 @@ import com.boondog.circulartetris.screens.GameScreen;
 
 public class CircularTetris extends Game {
 	public static final int worldWidth = 920, worldHeight = 720;
+	public static final int segments = 9;
+	public static final int blockWidth = 360/segments;
+	
 	
 	@Override
 	public void create() {
