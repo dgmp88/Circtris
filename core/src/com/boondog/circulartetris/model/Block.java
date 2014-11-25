d5b45d24c0b7b19a2382dfa65595e45e90052c0d
dgmp88 <dgmprichard@gmail.com>
2014-11-25 17:09:31 +0300

    Rotate left, rotate right, added color

--- core/src/com/boondog/circulartetris/model/Block.java
+++ core/src/com/boondog/circulartetris/model/Block.java
@@ -1,15 +1,28 @@
 package com.boondog.circulartetris.model;
 
+import java.util.Random;
+
+import com.badlogic.gdx.graphics.Color;
+import com.boondog.circulartetris.CircularTetris;
+import com.boondog.circulartetris.render.Colors;
+
 public class Block {
 	// Universal size of all blocks
-	public static final float blockHeight = 40, blockWidth = 360/12;
+	public static final float blockHeight = 40;
+	public static Random rand = new Random();
+	public static Colors colors = new Colors();
+	public static int colNum = 0;
 	
 	// Position of the block
 	float rotation, position;
 	
+	Color color;
+	
 	public Block(float rotation, float position) {
 		this.rotation = rotation;
 		this.position = position;
+		color = colors.get(colNum);
+		colNum++;
 	}
 	
 	
@@ -21,8 +34,19 @@ public class Block {
 		return position;
 	}
 	
-	public void rotate(float degrees) {
-		rotation += degrees;
+	public void rotateLeft() {
+		rotation += CircularTetris.blockWidth;
 	}
+
+	public void rotateRight() {
+		rotation -= CircularTetris.blockWidth;		
+	}
+	
+	public Color getColor() {
+		return color;
+	}
+
+
+
 	
 }
